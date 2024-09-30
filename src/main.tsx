ec49cb8089721633180aaf99c12c3e43f3d1083b
marshot <124557537+marshot9420@users.noreply.github.com>
2024-09-30 13:14:57 +0900

    feat: GlobalStyles 적용

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import { createRoot } from 'react-dom/client'
 
 import { ThemeProvider } from '@emotion/react'
 
-import { theme } from '@/styles'
+import { GlobalStyles, theme } from '@/styles'
 
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <App />
     </ThemeProvider>
   </StrictMode>,

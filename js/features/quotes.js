e09552bea96acab24cef11f8e7edc77cfb5750ea
marshot9420 <marshot9420@gmail.com>
2026-04-29 00:20:56 +0900

    feat: 인용문 출력 구현

---
+++ js/features/quotes.js
@@ -0,0 +1,70 @@
+const quoteContent = document.querySelector(".footer__quotes--content");
+const quoteAuthor = document.querySelector(".footer__quotes--author");
+
+let quotes = [
+  {
+    content:
+      "나는 무리 지어 다니는 사람들 중 제대로 된 인생을 사는 사람을 본 적이 없다",
+    author: "프리드리히 니체",
+  },
+  {
+    content:
+      "문제를 발생시켰을 때와 똑같은 의식수준으로는 어떤 문제도 해결할 수 없다",
+    author: "알버트 아인슈타인",
+  },
+  {
+    content: "분노는 나약함의 증거다",
+    author: "아리에텐",
+  },
+  {
+    content:
+      "우리가 세상을 바라보고 인식하는 것은 결국 믿음으로 이루어 져 있다",
+    author: "아리에텐",
+  },
+  {
+    content: "군중은 왕관에 박수치고, 왕의 목이 떨어질 땐 더 크게 환호한다",
+    author: "아리에텐",
+  },
+  {
+    content:
+      "돈이 없다고 해서 문제가 있는 삶이면, 돈이 있을 땐 더 큰 문제가 발생한다",
+    author: "아리에텐",
+  },
+  {
+    content:
+      "예수회는 신과 세상의 결합을 시도했지만, 결과적으로 신과 세상 모두에게 경멸을 받았다",
+    author: "블레즈 파스칼",
+  },
+  {
+    content: "이 세상은 잔혹하다. 그리고 너무나 아름답다",
+    author: "미카사 아커만",
+  },
+  {
+    content:
+      "무의식을 의식화 하지 못하면 무의식이 삶의 방향을 결정하게 되는데 사람들은 이를 두고 운명이라 부른다",
+    author: "카를 구스타프 융",
+  },
+  {
+    content: "동경은 이해에서 가장 먼 감정이다",
+    author: "아이젠 소스케",
+  },
+  {
+    content:
+      "세상은 인간이 설명할 수 있는 영역이 아니다. 하지만 소중한 이들과 온기를 나누면 모든 것이 이해 된다",
+    author: "아리에텐",
+  },
+  {
+    content: "똑똑하게 사랑하라",
+    author: "아리에텐",
+  },
+  {
+    content: "좋은 결정은 경험에서 나오고, 좋은 경험은 나쁜 결정에서 나온다",
+    author: "마크 트웨인",
+  },
+];
+
+export function getQuote() {
+  const randomQuoteItem = quotes[Math.floor(Math.random() * quotes.length)];
+  quoteContent.innerText = randomQuoteItem.content;
+  quoteAuthor.innerText = randomQuoteItem.author;
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import { getCurrentDate } from "./features/clock.js";
 import { handleLoginEvent } from "./features/login.js";
+import { getQuote } from "./features/quotes.js";
 import { controlMenuPanel } from "./layouts/panel.js";
 
 controlMenuPanel();
 getCurrentDate();
 handleLoginEvent();
+getQuote();
